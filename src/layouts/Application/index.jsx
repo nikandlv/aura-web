c3a43b21ec355d622bfaddf394f2b0f79c54a12c
Nikan Dalvand <nikandalvand@gmail.com>
2019-12-21 21:03:26 +0330

    Added a baseline for the project and cleaned up the cra boilerplate

---
+++ src/layouts/Application/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Wrapper from '../Wrapper'
+export default function Application() {
+    return (<Wrapper>
+        Hello there
+    </Wrapper>)
+}
---
+++ src/layouts/Wrapper/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Wrapper(props) {
+    return (
+        <section>
+            {props.children}
+        </section>
+    )
+}
